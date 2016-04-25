a2c69a147fa40ec2e63095c52fa84ea1e069da37
Austin DeLauney <austin@Austins-MacBook-Pro.local>
2016-04-25 12:50:56 -0400

    Last commit before revamping perculate up

--- MMHeap.cpp
+++ MMHeap.cpp
@@ -184,30 +184,92 @@ DataType MMheap<DataType>::deleteMax()
 template <typename DataType>
 void MMheap<DataType>::perculateUp(int index)
 {
-	int height = log(index) / log(2);
-	int grandparent = index >> 2;
-
-	cout << "Currently at height " << height << endl;
-
+	bool hasSwitched = false;
 	//If we are at root, or children of root, return
 	//(because we can't go any higher)
-	if(index < 4)
+	if(index < 3)
+	{
+		if(index == 0)
+		{
+			//cout << "YAY" << endl;
+			return;
+		}
+
+		int parent = (index - 1) / 2;
+
+		//cout << "Evaluating: " << heap[index] << " < " << heap[parent] << endl;
+		//PARENT SHOULD BE GREATER THAN
+		if(heap[parent] < heap[index])
+		{
+			//cout << "Swapping" << endl;
+			swap(parent, index);
+		}
+
 		return;
+	}
+
+	int height = (log(index) / log(2));
+	//cout << "Currently at height " << height << endl;
+	//cout << "index " << index << endl;
+	//cout << "calc  " << (log(index) / log(2)) << endl;
+	int parent = (index - 1) / 2;
+	int grandparent = (parent - 1) / 2;
+
+
+	//cout << "Index is : " << index << endl;
+	//cout << "grand par: " << grandparent << endl;
 
 	//If height is even
 	if(height % 2 == 0)
 	{
+		//cout << "Height is odd" << endl;
 
-		cout << "Height is even" << endl;
-
-		cout << "Evaluating: " << heap[index] << " < " << heap[grandparent] << endl;
+		//cout << "Evaluating: " << heap[index] << " < " << heap[grandparent] << endl;
 
 		//Check if we can perculate up
-		if(heap[grandparent] < heap[index])
+		if(heap[grandparent] > heap[index])
 		{
 
-			cout << "Swapping" << endl;
+			hasSwitched = true;
 
+			//cout << "Swapping" << endl;
+
+			//Swap them
+			swap(grandparent, index);
+
+			//Our index is now parent
+			index = grandparent;
+
+			//See if we can perculate up again
+			perculateUp(index);
+
+
+		}
+
+		if(!hasSwitched)
+		{
+			int parent = (index - 1) / 2;
+
+			//PARENT SHOULD BE GREATER THAN
+			if(heap[parent] > heap[index])
+				swap(parent, index);
+		}
+
+		//Else: We can't perculateUp so continue this method to exit
+	}
+
+	//Height is odd
+	else
+	{
+		//cout << "Height is odd" << endl;
+		//Check if we perculate up
+
+		//cout << "Evaluating: " << heap[index] << " < " << heap[grandparent] << endl;
+
+		if(heap[grandparent] > heap[index])
+		{
+			hasSwitched = true;
+			//cout << "Swapping" << endl;
 			//Swap them
 			swap(grandparent, index);
 
@@ -218,28 +280,14 @@ void MMheap<DataType>::perculateUp(int index)
 			perculateUp(index);
 		}
 
-		//Else: We can't perculateUp so continue this method to exit
-	}
-
-	//Height is odd
-	else
-	{
-		cout << "Height is odd" << endl;
-		//Check if we perculate up
-
-		cout << "Evaluating: " << heap[index] << " < " << heap[grandparent] << endl;
-
-		if(heap[index] < heap[grandparent])
+		if(!hasSwitched)
 		{
-			cout << "Swapping" << endl;
-			//Swap them
-			swap(grandparent, index);
 
-			//Our index is now parent
-			index = grandparent;
+			int parent = (index - 1) / 2;
 
-			//See if we can perculate up again
-			perculateUp(index);
+			//PARENT SHOULD BE SMALLER THAN
+			if(heap[parent] < heap[index])
+				swap(parent, index);
 		}
 
 		//Else: We can't perculateUp so we continue this method to exit
@@ -249,29 +297,35 @@ void MMheap<DataType>::perculateUp(int index)
 template <typename DataType>
 void MMheap<DataType>::perculateDown(int index)
 {
-	int grandchildren = index << 2;
+	int child = (index * 2) + 1;
+	int grandchildren = (child * 2) + 1;
 	int height = log(index) / log(2);
 
-	//We are on an even height
+	//We are on an ODD height (root is level 1)
 	if(height % 2 == 0)
 	{
 		//We have all of our grandchildren
 		if((grandchildren + 4) < currentIndex)
 		{
+			cout << "heap at index: " << heap[index] << endl;
+			cout << "grandchildren: " << endl;
 			//Get the smallest one
 			int smallestGrandchild = grandchildren;
-
+			cout << heap[smallestGrandchild] << endl;
 
 			for(int i = grandchildren + 1; i < grandchildren + 4; i++)
 			{
+				cout << heap[grandchildren + i] << endl;
 				if(heap[smallestGrandchild] > heap[i])
 					smallestGrandchild = i;
 			}
 
+			cout << "smallest grandchild is " << heap[smallestGrandchild];
+
 			//Base case: We're in the right position so check parent, and return
 			if(heap[index] < heap[smallestGrandchild])
 			{
-				int parent = index >> 1;
+				int parent = index / 2;
 
 				//We should be less than parent
 				if(heap[index] > heap[parent])
@@ -324,7 +378,7 @@ void MMheap<DataType>::perculateDown(int index)
 				index = absMin;
 			}
 
-			int parent = index >> 1;
+			int parent = (index - 1) / 2;
 
 			if(heap[index] < heap[parent])
 			{
@@ -333,6 +387,7 @@ void MMheap<DataType>::perculateDown(int index)
 		}
 	}
 
+	//We're on an EVEN height, root starts at height 1
 	else
 	{
 		//We have all of our grandchildren
@@ -351,7 +406,7 @@ void MMheap<DataType>::perculateDown(int index)
 			//Base case: We're in the right position so check parent, and return
 			if(heap[index] > heap[biggestGrandchild])
 			{
-				int parent = index >> 1;
+				int parent = (index - 1) / 2;
 
 				//We should be less than parent
 				if(heap[index] > heap[parent])
@@ -404,7 +459,7 @@ void MMheap<DataType>::perculateDown(int index)
 				index = absMax;
 			}
 
-			int parent = index >> 1;
+			int parent = (index - 1) / 2;
 
 			if(heap[index] > heap[parent])
 			{
@@ -442,8 +497,8 @@ int MMheap<DataType>::getMaxTwoGen(int index)
 	vector<DataType> descendents;
 	vector<int> 	 indexes;
 
-	int children = index << 1;
-	int grandchild = index << 2;
+	int children = (index * 2) + 1;
+	int grandchild = (children * 2) + 1 ;
 
 	//For both children
 	for(int i = 0; i < 2; i++)
@@ -572,8 +627,8 @@ int MMheap<DataType>::getMinTwoGen(int index)
 	vector<DataType> descendents;
 	vector<int> 	 indexes;
 
-	int children = index << 1;
-	int grandchild = index << 2;
+	int children = (index * 2) + 1;
+	int grandchild = (children * 2) + 1;
 
 	//For both children
 	for(int i = 0; i < 2; i++)
--- TestDriver.cpp
+++ TestDriver.cpp
@@ -16,8 +16,16 @@ int main()
 	theHeap.insert(3);
 	theHeap.insert(4);
 	theHeap.insert(5);
-	theHeap.insert(99);
-
+	theHeap.insert(6);
+	theHeap.insert(7);
+	theHeap.insert(8);
+	/*theHeap.insert(9);
+	theHeap.insert(10);
+	theHeap.insert(11);
+	theHeap.insert(12);
+	theHeap.insert(13);
+		theHeap.insert(14);	theHeap.insert(15);	theHeap.insert(16);	theHeap.insert(17);	
+*/
 	theHeap.dump();
 }
 
